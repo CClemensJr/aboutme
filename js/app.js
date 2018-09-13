bb3213511912d517e7b97042be1a37b0d460acca
Nicholas Crain <nicholascrain@gmail.com>
2018-09-13 15:31:28 -0700

    moved the switch statement from askQuestions function into its own checkAnswers function.

--- js/app.js
+++ js/app.js
@@ -1,145 +1,164 @@
 'use strict';
 
-var questions =         ['Do you think I like to wear sweaters?',
-                         'Do you think I like to watch football?',
-                         'Do you think I like to #netflixnchill?',
-                         'Do you think I am hungry?',
-                         'Do you think that I think you think web development is super special?',
-                         'How many martial arts do you think I have trained in?'];
-var answers =           [];
-var responses =         ['That is correct!', 'That is not the correct answer'];
-var guesses =           0;
-var score =             0;
+var questions = ['Do you think I like to wear sweaters?',
+                 'Do you think I like to watch football?',
+                 'Do you think I like to #netflixnchill?',
+                 'Do you think I am hungry?',
+                 'Do you think that I think you think web development is super special?',
+                 'How many martial arts do you think I have trained in?'];
+var answers =   [];
+var responses = ['That is correct!', 'That is not the correct answer'];
+var guesses =   0;
+var score =     0;
 
-alert('Hi, my name is Charles.');
-alert('This is a little quiz about me.')
-confirm('Are you ready?');
-
-for (var i = 0; i < questions.length; i++)
+function askQuestions() 
 {
-    answers[i] = prompt(questions[i]).toUpperCase();
+    alert('Hi, my name is Charles.');
+    alert('This is a little quiz about me.')
+    confirm('Are you ready?');
 
-    console.log('The user answered ' + answers[i] + ' to Question ' + (i + 1) + '. ' + questions[i]);
-
-    switch (i)
+    for (var i = 0; i < questions.length; i++)
     {
-        case 0:
-            if ((answers[0] === 'Y' || answers[0] === 'YES'))
-            {
-                alert(responses[0]);
-                score++;
+        answers[i] = prompt(questions[i]).toUpperCase();
 
-                break;
-            }
-            else
-            {
-                alert(responses[1]);
+        console.log('The user answered ' + answers[i] + ' to Question ' + (i + 1) + '. ' + questions[i]);
 
-                break;
-            }
+        checkAnswers(i);
 
-        case 1:
-            if ((answers[1] === 'N' || answers[1] === 'NO'))
-            {
-                alert(responses[0]);
-                score++;
-
-                break;
-            }
-            else
-            {
-                alert(responses[1]);
-
-                break;
-            }
-
-        case 2:
-            if ((answers[2] === 'N' || answers[2] === 'NO'))
-            {
-                alert(responses[0]);
-                score++;
-
-                break;
-            }
-            else
-            {
-                alert(responses[1]);
-
-                break;
-            }
-
-        case 3:
-            if ((answers[3] === 'Y' || answers[3] === 'YES'))
-            {
-                alert(responses[0]);
-                score++;
-
-                break;
-            }
-            else
-            {
-                alert(responses[1]);
-
-                break;
-            }
-
-        case 4:
-            if ((answers[4] === 'Y' || answers[4] === 'YES'))
-            {
-                alert(responses[0]);
-                score++;
-
-                break;
-            }
-            else
-            {
-                alert(responses[1]);
-
-                break;
-            }
-
-        case 5:
-            if (Number(answers[5]) === 5)
-            {
-                alert(responses[0]);
-                score++;
-
-                break;
-            }
-            else
-            {
-                do
-                {
-                    if (Number(answers[5]) < 5)
-                    {
-                        alert('That is too low. Please guess again.');
-
-                        answers[5] = prompt(questions[i]).toUpperCase();
-                        
-                        guesses++;
-                    }
-                    else if (Number(answers[5]) > 5)
-                    {
-                        alert('That is too high. Please guess again.');
-
-                        answers[5] = prompt(questions[i]).toUpperCase();
-                        
-                        guesses++;
-                    }
-                    else
-                    {
-                        alert(responses[0]);
-                        score++;
-
-                        break;
-                    }
-                } while (guesses < 4);
-            }  
-            break;
     }
 
+    alert('It will be a moment while I tally your results.'); 
+
+    alert('You scored ' + score + ' out of 5 correctly.');
 }
 
-alert('It will be a moment while I tally your results.'); 
 
-alert('You scored ' + score + ' out of 5 correctly.');
+function checkAnswers(num) 
+{
+    switch (num)
+        {
+            case 0:
+                if ((answers[0] === 'Y' || answers[0] === 'YES'))
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    alert(responses[1]);
+
+                    break;
+                }
+
+            case 1:
+                if ((answers[1] === 'N' || answers[1] === 'NO'))
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    alert(responses[1]);
+
+                    break;
+                }
+
+            case 2:
+                if ((answers[2] === 'N' || answers[2] === 'NO'))
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    alert(responses[1]);
+
+                    break;
+                }
+
+            case 3:
+                if ((answers[3] === 'Y' || answers[3] === 'YES'))
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    alert(responses[1]);
+
+                    break;
+                }
+
+            case 4:
+                if ((answers[4] === 'Y' || answers[4] === 'YES'))
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    alert(responses[1]);
+
+                    break;
+                }
+
+            case 5:
+                if (Number(answers[5]) === 5)
+                {
+                    alert(responses[0]);
+                    score++;
+
+                    break;
+                }
+                else
+                {
+                    do
+                    {
+                        if (Number(answers[5]) < 5)
+                        {
+                            alert('That is too low. Please guess again.');
+
+                            answers[5] = prompt(questions[num]).toUpperCase();
+                            
+                            guesses++;
+                        }
+                        else if (Number(answers[5]) > 5)
+                        {
+                            alert('That is too high. Please guess again.');
+
+                            answers[5] = prompt(questions[num]).toUpperCase();
+                            
+                            guesses++;
+                        }
+                        else if (Number(answers[5]) === 5)
+                        {
+                            alert(responses[0]);
+                            score++;
+
+                            break;
+                        }
+                        else
+                        {
+                            alert('Please enter a number.')
+
+                            answers[5] = prompt(questions[num]).toUpperCase();
+
+                            guesses++;
+                        }
+                    } while (guesses < 3);
+                }  
+                break;
+        }
+}
+
+askQuestions();
