ef9a5d3cf7b60f0a1acc2c1b6c8c84bcab76d9ca
Charles Clemens <cclemensjr@gmail.com>
2018-09-13 08:06:39 -0700

    Added an image and worked on question logic

--- js/app.js
+++ js/app.js
@@ -26,3 +26,107 @@
 // Expect that you will probably need both 'for' and 'while' structures for your sixth and seventh questions. But not necessarily.
 
 "use strict";
+
+var questions =         ['Do you think I like to wear sweaters?',
+                         'Do you think I like to watch football?',
+                         'Do you think I like to #netflixnchill?',
+                         'Do you think I am hungry?',
+                         'Do you think that I think you think web development is super special?'];
+var answers =           [];
+var responses =         ['That is correct!', 'That is not the correct answer'];
+var score =             0;
+
+alert('Hi, my name is Charles.');
+alert('This is a little quiz about me.')
+confirm('Are you ready?');
+
+for (var i = 0; i < questions.length; i++)
+{
+    answers[i] = prompt(questions[i]).toUpperCase();
+
+    console.log('The user answered ' + answers[i] + ' to Question ' + (i + 1) + '. ' + questions[i]);
+
+    switch (i)
+    {
+        case 0:
+            if ((answers[0] === 'Y' || answers[0] === 'YES'))
+            {
+                alert(responses[0]);
+                score++;
+
+                break;
+            }
+            else
+            {
+                alert(responses[1]);
+
+                break;
+            }
+
+        case 1:
+            if ((answers[1] === 'N' || answers[1] === 'NO'))
+            {
+                alert(responses[0]);
+                score++;
+
+                break;
+            }
+            else
+            {
+                alert(responses[1]);
+
+                break;
+            }
+
+        case 2:
+            if ((answers[2] === 'N' || answers[2] === 'NO'))
+            {
+                alert(responses[0]);
+                score++;
+
+                break;
+            }
+            else
+            {
+                alert(responses[1]);
+
+                break;
+            }
+
+        case 3:
+            if ((answers[3] === 'Y' || answers[3] === 'YES'))
+            {
+                alert(responses[0]);
+                score++;
+
+                break;
+            }
+            else
+            {
+                alert(responses[1]);
+
+                break;
+            }
+
+        case 4:
+            if ((answers[4] === 'Y' || answers[4] === 'YES'))
+            {
+                alert(responses[0]);
+                score++;
+
+                break;
+            }
+            else
+            {
+                alert(responses[1]);
+
+                break;
+            }
+
+    }
+
+}
+
+alert('It will be a moment while I tally your results.'); 
+
+alert('You scored ' + score + ' out of 5 correctly.');
